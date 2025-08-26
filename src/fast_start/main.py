28e916e1186fc024afc5ab49e8fb0504692edee3
sandrorsjunior <sandrorsjunior@outlook.com>
2025-08-26 22:17:53 +0100

    add new router

--- src/fast_start/main.py
+++ src/fast_start/main.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 
 from .controller.FileController import FileController
 from .controller.UserController import UserController
+from .controller.TokenController import TokenController
 from .config.database import Base, engine  
 
 Base.metadata.create_all(bind=engine)
 
 user_controller = UserController()
 file_controller = FileController()
+token_controller = TokenController()
+
 app = FastAPI()
 app.include_router(user_controller.router)
 app.include_router(file_controller.router)
+app.include_router(token_controller.router)
